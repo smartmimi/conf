a0d7a3b2d7f9719c1c547c9db9621284840410c9
smartmimi <43767309+smartmimi@users.noreply.github.com>
2021-02-26 10:19:40 +0800

    Update DataQuery.js

--- surge/DataQuery.js
+++ surge/DataQuery.js
@@ -2,7 +2,7 @@
 无效，待施工
 */
 let headers = $response.headers;
-let headjson = JSON.stringify(headers);
+let headjson = JSON.parse(headers);
 let headflow = headjson["subscription-userinfo"];
 $notification.post("佩奇流量","查询",headflow);
 $done({});
