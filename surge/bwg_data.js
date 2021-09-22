9900908d0154ebb0fc887ccdd8f902b993ed7b8b
smartmimi <43767309+smartmimi@users.noreply.github.com>
2021-09-22 22:53:18 +0800

    Update bwg_data.js

--- surge/bwg_data.js
+++ surge/bwg_data.js
@@ -2,7 +2,7 @@ const veid = $persistentStore.read("bwgid") ;
 const api_key = $persistentStore.read("bwgkey") ;
 if (!(veid&&api_key)){
   $done({
-   title: "Bwg_Data",
+   title: "搬瓦工流量",
    style: "info",
    content: "请在boxjs中完善信息"
   })
@@ -12,7 +12,7 @@ $httpClient.get(url, function(error, response, data){
   let resp = JSON.parse(data)
   if (resp["error"]){
     $done({
-    title: "Bwg_Data",
+    title: "搬瓦工流量",
     style: "info",
      content: "api验证失败，请检查后重试"
     });
@@ -22,9 +22,9 @@ $httpClient.get(url, function(error, response, data){
     let datatotal = (resp["plan_monthly_data"] / (1024 * 1024 * 1024)).toFixed(0);
     let reset = redate(data_next_reset);
     $done({
-     title: "Bwg_Data",
+     title: "搬瓦工流量",
      icon : "airplane.circle.fill",
-     content: "已用： " + datares + "/"+datatotal+" G\n"+"重置： " + reset
+     content: "已用： " + datares + " / "+datatotal+" G\n"+"重置： " + reset
      });
     }
 });
