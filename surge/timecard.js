4ba65867b8341d8ada76170ce0191ce69b5c1adc
smartmimi <43767309+smartmimi@users.noreply.github.com>
2021-12-13 21:40:00 +0800

    Create timecard.js

---
+++ surge/timecard.js
@@ -0,0 +1,74 @@
+var tlist = {
+  1:  ["测试","2021-12-13"],
+  2: ["元旦", "2022-01-01"],
+  3: ["春节", "2022-02-01"],
+  3: ["元宵", "2022-02-15"],
+  4: ["清明", "2022-04-05"],
+  5: ["劳动", "2022-05-01"],
+  6: ["端午", "2022-06-03"],
+  7: ["中秋", "2022-09-10"]
+};
+let tnow = new Date();
+let tnowf =
+  tnow.getFullYear() + "-" + (tnow.getMonth() + 1) + "-" + tnow.getDate();
+
+/* 计算2个日期相差的天数，不包含今天，如：2016-12-13到2016-12-15，相差2天
+ * @param startDateString
+ * @param endDateString
+ * @returns
+ */
+function dateDiff(startDateString, endDateString) {
+  var separator = "-"; //日期分隔符
+  var startDates = startDateString.split(separator);
+  var endDates = endDateString.split(separator);
+  var startDate = new Date(startDates[0], startDates[1] - 1, startDates[2]);
+  var endDate = new Date(endDates[0], endDates[1] - 1, endDates[2]);
+  return parseInt(
+    (endDate - startDate) / 1000 / 60 / 60 / 24
+  ).toString();
+}
+
+//计算输入序号对应的时间与现在的天数间隔
+function tnumcount(num) {
+  let dnum = num;
+  return dateDiff(tnowf, tlist[dnum][1]);
+}
+
+//获取最接近的日期
+function now() {
+  for (var i = 1; i <= Object.getOwnPropertyNames(tlist).length; i++) {
+    if (Number(dateDiff(tnowf, tlist[i.toString()][1])) >= 0) {
+      //console.log("最近的日期是:" + tlist[i.toString()][0]);
+      //console.log("列表长度:" + Object.getOwnPropertyNames(tlist).length);
+      //console.log("时间差距:" + Number(dateDiff(tnowf, tlist[i.toString()][1])));
+      return i;
+    }
+  }
+}
+
+//如果是0天，发送emoji;
+let nowlist = now();
+function today(day) {
+  let daythis = day;
+  if (daythis == "0") {
+    datenotice();
+    return "🎉";
+  } else {
+    return daythis;
+  }
+}
+
+//提醒日当天发送通知
+function datenotice() {
+  if ($persistentStore.read("timecardpushed") != tlist[nowlist][1] && tnow.getHours() >= 6) {
+    $persistentStore.write(tlist[nowlist][1], "timecardpushed");
+    $notification.post("时间卡提醒", "今天是" + tlist[nowlist][1] + "日 " + tlist[nowlist][0] + " 🎉")
+  } else if ($persistentStore.read("timecardpushed") == tlist[nowlist][1]) {
+    console.log("当日已通知");
+  }
+}
+$done({
+title:"法定节假日，还有多少天？",
+icon:"calendar",
+content:tlist[nowlist][0]+"：     "+  today(tnumcount(nowlist))+"\n"+tlist[Number(nowlist) + Number(1)][0] +":     "+ tnumcount(Number(nowlist) + Number(1))+ "\n"+tlist[Number(nowlist) + Number(2)][0]+":     "+tnumcount(Number(nowlist) + Number(2))
+})
