01a3a2dae4b9cac01b8a5e1c427d436e097d0425
smartmimi-ios <jiangjunhu720@gmail.com>
2022-03-20 20:57:28 +0800

    更换图标

--- surge/timecard.js
+++ surge/timecard.js
@@ -73,8 +73,10 @@ function datenotice() {
 function icon_now(num){
   if(num<=7 && num>3 ){
     return "hare"
-  }else if(num<=3){
-    return "bolt"
+  }else if(num<=3 && num>=0){
+    return "timer"
+  }else if(num==0){
+    return "gift"
   }else{
     return "tortoise"
   }
