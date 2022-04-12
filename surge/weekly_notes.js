aafbc5e49d8bae8fdaeabe386824519bfb41d64c
smartmimi <43767309+smartmimi@users.noreply.github.com>
2022-04-12 11:11:42 +0800

    Create weekly_notes.js

---
+++ surge/weekly_notes.js
@@ -0,0 +1,16 @@
+const weekly_notes_boxjs_title = $persistentStore.read("weekly_notes_title");
+const weekly_notes_boxjs = {};
+//拼接boxjs内容
+if(weekly_notes_boxjs_title){
+  for(i=0;i<=6;i++){
+    weekly_notes_boxjs[i] = $persistentStore.read(`weekly_notes_${i}`);
+  }
+}
+
+var tnow = new Date();
+var tnoww = tnow.getDay();
+$done({
+title:"周"+`${tnoww==0?"日":tnoww}`+"信息提醒",
+icon:greetingcard,
+content:tlist[tnow]
+})
