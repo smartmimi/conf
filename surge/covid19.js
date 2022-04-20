e52ac532ac9e62fd45af9cab287611297092c0e1
smartmimi <43767309+smartmimi@users.noreply.github.com>
2022-04-20 08:10:46 +0800

    Update covid19.js

--- surge/covid19.js
+++ surge/covid19.js
@@ -11,7 +11,8 @@ if (!read){
 };
 function nowtime(){
  let now = new Date();
- let time = now.getHours()+":"+now.getMinutes()+":"+now.getSeconds();
+ let time = now.getHours()+":"+now.getMinutes();
+ // +":"+now.getSeconds();
  return time
 }
 function num(location, result) {
@@ -19,11 +20,13 @@ function num(location, result) {
   var resu = result;
   var loc_new = new RegExp(loc + "[\\s\\S]*?confirm[\\s\\S]{3}(\\d+)");
   var loc_now = new RegExp(loc + "[\\s\\S]*?nowConfirm[\\s\\S]{3}(\\d+)");
+  var loc_wzz = new RegExp(loc + "[\\s\\S]*?wzz[\\s\\S]{3}(\\d+)");
   let loc_new_res = loc_new.exec(resu);
   let loc_now_res = loc_now.exec(resu);
+  let loc_wzz_res = loc_wzz.exec(resu);
   if (loc_new_res) {
     //console.log("已获取" + loc + "的信息");
-    ala = ala +loc +"   :   " +loc_new_res[1].padStart(5,"\u0020")+"/" +loc_now_res[1].padStart(5,"\u0020")+ "\n";
+    ala = ala +loc +"   :   " +loc_new_res[1].padStart(6,"\u0020")+"|" +loc_now_res[1].padStart(6,"\u0020")+ loc_wzz_res[1].padStart(6,"\u0020")+"\n";
   } else {
     //console.log("获取" + loc + "的信息失败");
     ala = ala + loc + "   :   查无数据\n";
@@ -35,7 +38,7 @@ $httpClient.get(url, function(error, response, data){
     num(list[i], res);
     if (i == list.length - 1) {
      $done({
-       title: "疫情查询:新增 / 现存"+ "   "+nowtime(),
+       title: "疫情:新增 | 现存 | 无症状"+ "   "+nowtime(),
        icon : "heart.circle",
        content: ala.replace(/\n$/, "")
      });
