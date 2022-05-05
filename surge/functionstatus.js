0958ecbe71f4e1fb8e85707950e4f7aa9142431f
smartmimi <43767309+smartmimi@users.noreply.github.com>
2022-05-05 20:21:46 +0800

    Update functionstatus.js

--- surge/functionstatus.js
+++ surge/functionstatus.js
@@ -9,13 +9,12 @@ function httpAPI(path = "", method = "POST", body = null) {
 let mitm_status = (await httpAPI("/v1/features/mitm","GET"));
 let rewrite_status = (await httpAPI("/v1/features/rewrite","GET"));
 let scripting_status = (await httpAPI("/v1/features/scripting","GET"));
-  $done({
-      title:"功能开关显示",
-      content:"Mitm："+icon_status(mitm_status.enabled)+" Rewrite:"+icon_status(rewrite_status.enabled)+" Scripting:"+icon_status(scripting_status.enabled),
-      icon: "gearshape",
-     // "icon-color":params.color
-    });
-
+$done({
+    title:"功能开关显示",
+    content:"Mitm："+icon_status(mitm_status.enabled)+" Rewrite:"+icon_status(rewrite_status.enabled)+" Scripting:"+icon_status(scripting_status.enabled),
+    icon: "gearshape",
+   // "icon-color":params.color
+});
 })();
 function icon_status(status){
   if (status){
