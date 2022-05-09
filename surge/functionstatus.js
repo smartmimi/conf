20792fea4553465bce524bd80d0559b9da6ae310
smartmimi <43767309+smartmimi@users.noreply.github.com>
2022-05-09 15:16:50 +0800

    Update functionstatus.js

--- surge/functionstatus.js
+++ surge/functionstatus.js
@@ -19,7 +19,7 @@ $done({
     title:"Surge  已运行"+startTime,
     content:"Mitm:"+icon_status(mitm_status.enabled)+"  Rewrite:"+icon_status(rewrite_status.enabled)+"  Scripting:"+icon_status(scripting_status.enabled),
     icon: icon_s?"checkmark.seal":"exclamationmark.triangle",
-   "icon-color":icon_s?"#16A951":"#FAFF72"
+   "icon-color":icon_s?"#16A951":"#FF7500"
 });
 })();
 function icon_status(status){
